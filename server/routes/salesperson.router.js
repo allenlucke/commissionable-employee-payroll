6521570fed59af98b73ec16f2a4bb43134a06abf
Allen Lucke <allenlucke@gmail.com>
2020-01-17 17:24:42 -0600

    added nested get to add sales router for salesperson add sales page

--- server/routes/salesperson.router.js
+++ server/routes/salesperson.router.js
@@ -61,19 +61,31 @@ router.post('/addSale', (req, res) => {
         console.log(response.rows.map((item, index) => {
             return item.id;
         }))
-        res.send(response.rows.map((item, index) => {
+        response.rows.map((item, index) => {
             return newSalesId = item.id;
-        }))
+        })
+        // res.send(response.rows.map((item, index) => {
+        //     return newSalesId = item.id;
+        // }))
         console.log(newSalesId)
         const queryString = `INSERT INTO "sales_products" ("sales_id", "product_id", "unitsSold")
         VALUES (${newSalesId}, ${product_id}, ${unitsSold});`;
         pool.query(queryString)
         .then((response2) => {
-            // res.sendStatus(201)
-            
+            const queryString = `SELECT "employees".id, "employees"."bonusTier", 
+            SUM("sales_products"."unitsSold") AS "totalProductsSold" FROM "employees"
+            JOIN "sales" ON "employees".id = "sales".employees_id
+            JOIN "sales_products" ON "sales".id = "sales_products".sales_id
+            WHERE "employees".id = ${userID}
+            GROUP BY "employees".id;`;
             pool.query(queryString)
             .then((response3) => {
-
+                console.log(response3.rows)
+                // res.send({
+                //     response2: response2.rows,
+                //     response3: response3.rows,
+                // })
+                res.send(response3.rows)
             })
             .catch((err) => {
                 res.sendStatus(500);
